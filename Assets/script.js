77a9d7011e48cbfa11be8534a1208acb55d3c71f
arjun gupta <arjunkarangupta100@gmail.com>
2022-10-17 23:53:56 -0400

    Commit after getting help from a tutor, added proper styling with JS to avoid hardcoding in HTML

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,70 @@
-//just console log SOMETHING
-var idiotInput = document.querySelectorAll(idiotInput);
+//just console log SOMETHING the moment time 
+// var idiotInput = document.querySelectorAll(idiotInput);
+var saveButton = document.getElementById("save");
+var timeBlock = document.getElementById("TimeBlocks")
+
 var now = moment();
 console.log(now);
 
+
+//displays the date using JQuery
 todaysDate = $("#currentDay");
 function displayDate() {
-    var today = moment()
-    todaysDate.text(today);
+  var today = moment()
+  todaysDate.text(today);
 
-  }
+}
 displayDate()
 
+function renderTimeBlocks() {
+  for (let i = 9; i < 18; i++) {
+    var time = i;
+    if(time>12){
+      time -= 12;
+      time += " PM";
+    }
+    else{
+      time += " AM";
+    }
+    var thisCard = `
+    <div class="time-block row">
+      <h3 class="col-1">${time}</h3>
+      <textarea class="inputI col-10"></textarea>
+        <button class="save col-1">SAVE</button>
+    </div> 
+`
+timeBlock.innerHTML += thisCard
+  }
+addListeners();
+}
+renderTimeBlocks();
+function addListeners() {
+let saveButtons = document.getElementsByClassName("save");
+for (let i = 0; i < saveButtons.length; i++) {
+  const element = saveButtons[i];
+  element.addEventListener("click", function(e){
+    console.log(e);
+  })
+}
+};
 
 
+saveButton.addEventListener("click", function (event) {
+  event.preventDefault();
 
+})
+// function saveInput(){
+//   var idiotsInput = localStorage.getItem('Some text...');
+//   localStorage.setItem('myText', idiotsInput);
+// }
 
-
-// var a = moment().toString(); 
+// function get() {
+//   var storedValue = localStorage.getItem('text');
+//   if(storedValue) {
+//       document.getElementById('textfield').value = storedValue;
+//   }
+// }
+// var a = moment().toString();
 // document.getElementById("todaysdate") = a
 
 // console.log("time");
@@ -28,11 +76,14 @@ displayDate()
 
 // $( function() {
 //     $( "#selectable" ).selectable();
-//   } );
+//  }
+// trying to set up local storage
+// var UUP = ["bruh", "wtf"];
 
-$( function() {
-  $( ".widget input[type=submit], .widget a, .widget button" ).button();
-  $( "button, input, a" ).on( "click", function( event ) {
-    event.preventDefault();
-  } );
-} );
+// let inputObj = JSON.stringify(UUP);
+
+// localStorage.setItem("UUP", inputObj);
+
+// let inputObjBack = JSON.parse(localStorage.getItem(UUP));
+
+// console.log(inputObjBack);
